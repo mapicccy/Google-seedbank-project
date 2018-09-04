1888d83d37c5a084944a19286213f218fac0b20c
zhaoguanjun <773216251@qq.com>
2018-09-04 17:15:33 +0800

    simple tensorflow example

--- basicML/tensorflow_hello_world.py
+++ basicML/tensorflow_hello_world.py
@@ -1,5 +1,12 @@
+from __future__ import print_function
 import tensorflow as tf
 
+try:
+    tf.contrib.eager.enable_eager_execution()
+except ValueError:
+    pass
+
+
 c = tf.constant('hello world!')
-with tf.Session() as sess:
+with tf.Session(graph=tf.get_default_graph) as sess:
     print(sess.run(c))
