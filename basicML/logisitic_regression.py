f45de4dca20f6cd1060098670d0c4c79840071bd
mapicccy <773216251@qq.com>
2018-09-25 17:30:35 +0800

    Update logisitic_regression.py

--- basicML/logisitic_regression.py
+++ basicML/logisitic_regression.py
@@ -20,3 +20,67 @@ california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mle
 
 california_housing_dataframe = california_housing_dataframe.reindex(
     np.random.permutation(california_housing_dataframe.index))
+
+
+def preprocess_features(california_housing_dataframe):
+    """Prepares input features from California housing data set.
+
+  Args:
+    california_housing_dataframe: A Pandas DataFrame expected to contain data
+      from the California housing data set.
+  Returns:
+    A DataFrame that contains the features to be used for the model, including
+    synthetic features.
+  """
+    selected_features = california_housing_dataframe[
+        ["latitude",
+         "longitude",
+         "housing_median_age",
+         "total_rooms",
+         "total_bedrooms",
+         "population",
+         "households",
+         "median_income"]]
+    processed_features = selected_features.copy()
+    # Create a synthetic feature.
+    processed_features["rooms_per_person"] = (
+            california_housing_dataframe["total_rooms"] /
+            california_housing_dataframe["population"])
+    return processed_features
+
+
+def preprocess_targets(california_housing_dataframe):
+    """Prepares target features (i.e., labels) from California housing data set.
+
+  Args:
+    california_housing_dataframe: A Pandas DataFrame expected to contain data
+      from the California housing data set.
+  Returns:
+    A DataFrame that contains the target feature.
+  """
+    output_targets = pd.DataFrame()
+    # Create a boolean categorical feature representing whether the
+    # median_house_value is above a set threshold.
+    output_targets["median_house_value_is_high"] = (
+            california_housing_dataframe["median_house_value"] > 265000).astype(float)
+    return output_targets
+
+
+# Choose the first 12000 (out of 17000) examples for training.
+training_examples = preprocess_features(california_housing_dataframe.head(12000))
+training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+
+# Choose the last 5000 (out of 17000) examples for validation.
+validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
+validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
+
+# Double-check that we've done the right thing.
+print("Training examples summary:")
+display.display(training_examples.describe())
+print("Validation examples summary:")
+display.display(validation_examples.describe())
+
+print("Training targets summary:")
+display.display(training_targets.describe())
+print("Validation targets summary:")
+display.display(validation_targets.describe())
